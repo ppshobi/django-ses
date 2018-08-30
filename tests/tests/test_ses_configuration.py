05d7bd79f3d6e364cf59497def68f2ba5fc39888
Shobi P P <ppshobi@gmail.com>
2018-08-31 00:45:51 +0530

    read AWS KEYS

--- tests/tests/test_ses_configuration.py
+++ tests/tests/test_ses_configuration.py
@@ -1,6 +1,10 @@
+from django.conf import settings
 from django.test import TestCase
-
+from moto import mock_ses
 
 class SesImportTest(TestCase):
-    def it_can_read_given_aws_keys(self):
-        pass
+
+    @mock_ses
+    def test_it_can_read_given_aws_keys(self,):
+        self.assertEquals(settings.AWS_ACCESS_KEY, 'xxx')
+        self.assertEquals(settings.AWS_SECRET_KEY, 'xxx')
