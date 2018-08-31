63d05160892f5da07cbf94756d1131bc506520e4
Shobi P P <ppshobi@gmail.com>
2018-08-31 17:26:02 +0530

    updated github url

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A Drop in Replacement for default django email backend based on Amazon SES",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/ppshobi/django-ses",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
