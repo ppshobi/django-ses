affa4c8910f6f6b9e852f06c911966e5c418fbed
Shobi P P <ppshobi@gmail.com>
2018-08-31 13:02:33 +0530

    test it doesnt raise exception when fail silently is used

--- tests/tests/test_ses_email.py
+++ tests/tests/test_ses_email.py
@@ -45,3 +45,20 @@ class SesEmail(TestCase):
         )
         self.assertRaises(ClientError, lambda: email1.send())
 
+    @mock_ses
+    def test_it_doesnt_raises_exception_if_fail_silently_is_true(self):
+        email1 = EmailMessage(
+            'Subject 1',
+            'Body1 goes here',
+            'from1@example.com',
+            ['to1@example.com', 'to2@example.com'],
+            reply_to=['another@example.com'],
+            headers={'Message-ID': 'foo'},
+        )
+        try:
+            email1.send(fail_silently=True)
+        except ClientError as e:
+            self.fail("it raised error even when fail silently is used")
+
+
+
