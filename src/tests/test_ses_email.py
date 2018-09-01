ad3f17405f00035cf82584e75201ca9549c08f23
Shobi P P <ppshobi@gmail.com>
2018-09-02 00:48:13 +0530

    mass email functionality

--- src/tests/test_ses_email.py
+++ src/tests/test_ses_email.py
@@ -2,7 +2,7 @@ import boto3
 from botocore.exceptions import ClientError
 from django.conf import settings
 from django.core import mail
-from django.core.mail import EmailMessage, send_mail
+from django.core.mail import EmailMessage, send_mail, send_mass_mail
 from django.core.mail.backends.base import BaseEmailBackend
 from django.test import TestCase
 from moto import mock_ses
@@ -47,6 +47,17 @@ class SesEmail(TestCase):
                               aws_secret_access_key=settings.AWS_SECRET_KEY)
         client.verify_email_identity(EmailAddress="from1@example.com")
         self.assertIsNotNone(send_mail(subject="Test Subject", message="Message", from_email="from1@example.com", recipient_list=['to@example.com']))
+
+    @mock_ses
+    def test_send_mass_email_function(self):
+        client = boto3.client('ses', region_name='us-east-1', aws_access_key_id=settings.AWS_ACCESS_KEY,
+                              aws_secret_access_key=settings.AWS_SECRET_KEY)
+        client.verify_email_identity(EmailAddress="from@example.com")
+
+        message1 = ('Subject here', 'Here is the message', 'from@example.com', ['first@example.com', 'other@example.com'])
+        message2 = ('Another Subject', 'Here is another message', 'from@example.com', ['second@test.com'])
+
+        self.assertIsNotNone(send_mass_mail((message1, message2), fail_silently=False))
 
     def test_it_can_return_an_email_backend(self):
         self.assertTrue(isinstance(django_ses.SesBackend(), BaseEmailBackend))
