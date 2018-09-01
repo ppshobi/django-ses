de002181e8a792b371b8c399cefc750978c327fc
Shobi P P <ppshobi@gmail.com>
2018-09-02 00:07:08 +0530

    added test for `send_email` function

--- src/tests/test_ses_email.py
+++ src/tests/test_ses_email.py
@@ -2,7 +2,7 @@ import boto3
 from botocore.exceptions import ClientError
 from django.conf import settings
 from django.core import mail
-from django.core.mail import EmailMessage
+from django.core.mail import EmailMessage, send_mail
 from django.core.mail.backends.base import BaseEmailBackend
 from django.test import TestCase
 from moto import mock_ses
@@ -40,6 +40,13 @@ class SesEmail(TestCase):
             email1.send(fail_silently=True)
         except ClientError as e:
             self.fail("it raised error even when fail silently is used")
+
+    @mock_ses
+    def test_send_email_function(self):
+        client = boto3.client('ses', region_name='us-east-1', aws_access_key_id=settings.AWS_ACCESS_KEY,
+                              aws_secret_access_key=settings.AWS_SECRET_KEY)
+        client.verify_email_identity(EmailAddress="from1@example.com")
+        self.assertIsNotNone(send_mail(subject="Test Subject", message="Message", from_email="from1@example.com", recipient_list=['to@example.com']))
 
     def test_it_can_return_an_email_backend(self):
         self.assertTrue(isinstance(django_ses.SesBackend(), BaseEmailBackend))
